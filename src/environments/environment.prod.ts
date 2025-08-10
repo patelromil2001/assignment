d48ca0898b630815594f5097e1f04d06be4c708c
patelromil.2001 <patelromil.2001@gmail.com>
2025-08-10 15:35:08 -0400

    Variable updates

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  SPOTIFY_CLIENT_ID: '',
-  SPOTIFY_REDIRECT_URI: '',
-  SPOTIFY_API_BASE: 'https://api.spotify.com/v1'
+  spotifyClientId: '926f984574974a64aae1d6005c1a181d',
+  spotifyRedirectUri: 'https://your-app-name.netlify.app/api', 
+  spotifyApiBase: 'https://api.spotify.com/v1'
 };

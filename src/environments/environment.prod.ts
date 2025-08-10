24c18a5c96ae1f103dc210517b68f6adf3769aab
patelromil.2001 <patelromil.2001@gmail.com>
2025-08-10 15:17:21 -0400

    Final Updates

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  SPOTIFY_CLIENT_ID: '',
+  SPOTIFY_REDIRECT_URI: '',
+  SPOTIFY_API_BASE: 'https://api.spotify.com/v1'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  spotifyClientId: '926f984574974a64aae1d6005c1a181d',
+  spotifyRedirectUri: 'http://localhost:4200/', 
+  spotifyApiBase: 'https://api.spotify.com/v1'
+};
